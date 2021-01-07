7402ebf761ea55bb6af05fd27061def2fa8995f2
paulius <paulius.gerve@protonmail.com>
2021-01-07 18:11:43 +0200

    lint

--- website/views.py
+++ website/views.py
@@ -7,8 +7,10 @@ from expenses.models import Expense
 
 @login_required
 def index(request):
-    ammount_expenses = Expense.objects.filter(owner=request.user).count()
-    total_amount =  Expense.objects.filter(owner=request.user).aggregate(Sum("amount"))["amount__sum"]
+    amount_expenses = Expense.objects.filter(owner=request.user).count()
+    total_amount = Expense.objects.filter(owner=request.user).aggregate(Sum("amount"))[
+        "amount__sum"
+    ]
     total_spent = "{:.2f}".format(total_amount if total_amount else 0)
-    context = {"expenses": ammount_expenses, "total_spent": total_spent}
+    context = {"expenses": amount_expenses, "total_spent": total_spent}
     return render(request, "index.html", context=context)
